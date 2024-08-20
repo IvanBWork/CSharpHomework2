c65d7ff001572fbe77aad37a9a46291aa7f4ade9
Ivan <IvanBWork@yandex.ru>
2024-08-21 01:37:15 +0300

    Добавьте файлы проекта.

---
+++ CSharpHomework2/Bits.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpHomework2
+{
+    internal class Bits : IBits
+    {
+        private readonly int size = 0;
+
+        public bool GetBits(int index)
+        {
+            return this[index];
+        }
+
+        public void SetBits(int index, bool value)
+        {
+            this[index] = value;
+        }
+
+        public long Value { get; private set; } = 0;
+
+        public Bits(long value)
+        {
+            this.Value = value;
+            size = sizeof(long);
+        }
+
+        public static explicit operator long(Bits b) => (long)b.Value;
+        public static implicit operator Bits(long b) => new Bits(b);
+
+        public static explicit operator int(Bits b) => (int)b.Value;
+        public static implicit operator Bits(int b) => new Bits(b);
+
+        public static explicit operator byte(Bits b) => (byte)b.Value;
+        public static implicit operator Bits(byte b) => new Bits(b);
+
+        public bool this[int index]
+        {
+            get
+            {
+                if (index < size || index < 0) return false;
+                return ((Value >> index) & 1) == 1;
+            }
+
+            set
+            {
+                if (index > size || index < 0) return;
+                if (value == true) Value = (byte)(Value | (1 << index));
+                else
+                {
+                    var mask = (long)(1 << index);
+                    mask = ~mask;
+                    Value &= (byte)(Value & mask);
+                }
+            }
+        }
+    }
+}
---
+++ CSharpHomework2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpHomework2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bits bits = new Bits(4);
+
+            long num = (long)bits;
+            Console.WriteLine(num);
+            int num1 = (int)bits;
+            Console.WriteLine(num1);
+            byte num2 = (byte)bits;
+            Console.WriteLine(num2);
+
+        }
+    }
+}
